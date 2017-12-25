81cb55ad28c83a6da16eb9bc8fc8e912d9258f44
sunxiuqiao <yanhejin@163.com>
2017-12-25 10:14:57 +0800

    ui

--- src/index.js
+++ src/index.js
@@ -4,21 +4,40 @@ import 'cesium/Widgets/widgets.css'
 import './static/css/index.css'
 
 import {addTerrain} from './modules/addTerrain'
+/*import {addEntity} from './modules/addEntity'
+*//*import {addDixing} from './modules/addLayer'
+import {addImage} from './modules/addLayer'*/
+
+/*import {add} from './modules/addLayers'*/
 
 let viewerOption = {
-    animation: false,
-    timeline: false,
-    navigationInstructionsInitiallyVisible: false,
+    animation: true,
+    timeline: true,
+    navigationInstructionsInitiallyVisible: true,
     navigationHelpButton: false,
     sceneModePicker: false,
     baseLayerPicker: false,
     infoBox: false,
-    selectionIndicator: false,
-    clock: null
+    selectionIndicator: true,
+    clock: null,
+    vrButton : true,
+    scene3DOnly:true,
+    geocoder:true,//是否显示geocoder小器件，右上角查询按钮    
+    fullscreenButton:true,//是否显示全屏按钮  
+    selectedTerrainProviderViewModel : undefined,//当前地形图层的显示模型，仅baseLayerPicker设为true有意义   
+    terrainProviderViewModels : Cesium.createDefaultTerrainProviderViewModels(),//可供BaseLayerPicker选择的地形图层ProviderViewModel数组    
+    imageryProviderViewModels : Cesium.createDefaultImageryProviderViewModels(),//可供BaseLayerPicker选择的图像图层ProviderViewModel数组
+
 }
 
-const viewer = new Cesium.Viewer('cesium-container', viewerOption)
+const viewer = new Cesium.Viewer('cesium-container',viewerOption)
 
 addTerrain(viewer)
+/*add(Viewer)*/
+console.log(viewer.entities)
+/*addEntity(viewer)*/
+/*addImage(viewer)
+console.log(viewer.entities)*/
+
 
 console.log($('#cesium-container'))
--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,8 @@ module.exports = {
             template: 'src/index.html'
         }),
         new CopywebpackPlugin([{from: path.join(cesiumSource, cesiumWorkers), to: 'Workers'}]),
-        // new CopywebpackPlugin([{from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
-        // new CopywebpackPlugin([{from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
+        new CopywebpackPlugin([{from: path.join(cesiumSource, 'Assets'), to: 'Assets'}]),
+        new CopywebpackPlugin([{from: path.join(cesiumSource, 'Widgets'), to: 'Widgets'}]),
         new webpack.DefinePlugin({
             // Define relative base path in cesium for loading assets
             CESIUM_BASE_URL: JSON.stringify('')
